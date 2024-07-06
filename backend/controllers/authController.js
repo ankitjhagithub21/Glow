69f21ebfab132c3e49d353e3b4a14654b0bcd674
ankitjhagithub21 <ajha45075@gmail.com>
2024-07-06 06:17:11 +0530

    change logout function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -119,9 +119,7 @@ const login = async (req, res) => {
 
 const logout = async (req, res) => {
     try {
-       res.clearCookie('jwt',{
-        maxAge:0
-       })
+        res.cookies().set('jwt', '', { maxAge: 0 })
        
        res.status(200).json({ success: true, message: "Logout successfull." })
     } catch (error) {
